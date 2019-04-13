72b8acd71065e724f1c361c088bc45ba651552ff
Alex Tagun <aleksejtagun@yandex.ru>
2019-04-13 18:25:09 +0300

    added TextMeshPro asset, building buildings. new scene "City"

---
+++ Assets/CarCity/Scripts/BuildTimer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BuildTimer : MonoBehaviour
+{
+    private TextMeshPro _text;
+    private float _currCountdownValue;
+
+    private void Awake() {
+        _text = GetComponentInChildren<TextMeshPro>();
+    }
+
+    public void setTimer(float seconds) {
+        StartCoroutine(StartCountdown(seconds));
+    }
+
+    public IEnumerator StartCountdown(float countdownValue = 10) {
+        _currCountdownValue = countdownValue;
+        setText(_currCountdownValue);
+        while (_currCountdownValue > 0) {
+            yield return new WaitForSeconds(1.0f);
+            _currCountdownValue--;
+            setText(_currCountdownValue);
+        }
+        Destroy(gameObject);
+    }
+
+    private void setText(float value) {
+        int minutes = (int) value / 60;
+        int seconds = (int) value % 60;
+        string text = (minutes < 10) ? "0" + minutes : "" + minutes;
+        text += ':';
+        text += (seconds < 10) ? "0" + seconds : "" + seconds;
+        _text.text = text;
+    }
+
+    private void OnDestroy() {
+        GetComponentInParent<Builder>().OnTimerDestroy();
+    }
+}
---
+++ Assets/CarCity/Scripts/CameraMover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    private PolygonCollider2D _collider;
+    private bool _isPicked = false;
+    private Vector3 _screenPosition = Vector3.zero;
+    private Vector3 _oldPos = Vector3.zero;
+    
+    private float _panSpeedX = 17.55F;
+    
+    private float _panSpeedY = 10F;
+    void Start() {
+        _collider = GetComponent<PolygonCollider2D>();
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("DOWN");
+            _isPicked = true;
+            _oldPos = transform.position;
+            _screenPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            _isPicked = false;
+        }
+        Debug.Log(transform.position);
+        OnMouseMove();
+    }
+
+    private void OnMouseUp() {
+        _isPicked = false;
+    }
+
+    private void OnMouseMove() {
+        if (!_isPicked) return;
+        if (!HasMouseMoved()) return;
+        
+        Vector3 worldPos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - _screenPosition;
+
+        worldPos.x *= _panSpeedX;
+        worldPos.y *= _panSpeedY;
+        transform.position = _oldPos + -worldPos;
+    }
+
+    private bool HasMouseMoved() {
+        return (Input.GetAxis("Mouse X") != 0) || (Input.GetAxis("Mouse Y") != 0);
+    }
+}
