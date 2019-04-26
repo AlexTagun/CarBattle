3c086b178eb652f16d0bf93fbe15f06d5139cbbd
Alex Tagun <aleksejtagun@yandex.ru>
2019-04-26 21:54:32 +0300

    added trail after wheels, added new shader and materials for trails, some corrections in car prefab

---
+++ Assets/Units/Car/Logic/WheelTrailPainter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelTrailPainter : MonoBehaviour{
+
+    [SerializeField]
+    private GameObject _wheelTrailAsset;
+    [SerializeField]
+    private Material[] _materials;
+
+    private BoxCollider2D _collider;
+    private LineRenderer _lineRenderer = null;
+
+    void Start(){
+        _collider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update(){
+        UpdateTrail();
+        checkLand();
+
+
+
+    }
+
+    private void UpdateTrail() {
+        if (!_lineRenderer) return;
+        drawVerticy();
+    }
+
+    private void checkLand() {
+        RaycastHit2D hit = isOnCollider();
+        if (hit) {
+            //TODO indentification of such objects
+            OnTriggerEnter2D(hit.collider.gameObject.name);
+        } else {
+            OnTriggerExit2D();
+        }
+    }
+
+    private RaycastHit2D isOnCollider() {
+        Ray ray = new Ray(transform.position, Vector3.back);
+        return Physics2D.Raycast(ray.origin, ray.direction, 0, 1 << 8);
+    }
+
+    private LineRenderer startPaintTrail(Material material) {
+        GameObject wheelTrailObj = Instantiate(_wheelTrailAsset);
+        LineRenderer lineRenderer = wheelTrailObj.GetComponent<LineRenderer>();
+        lineRenderer.material = material;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position);
+        return wheelTrailObj.GetComponent<LineRenderer>();
+    }
+
+    private void drawVerticy() {
+        _lineRenderer.positionCount++;
+        _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, transform.position);
+    }
+
+    private void stopPaintTrail() {
+        _lineRenderer = null;
+    }
+
+    private void OnTriggerEnter2D(string land) {
+        Debug.Log("dada");
+        switch (land) {
+            case "sharpLand":
+                _lineRenderer = startPaintTrail(_materials[0]);
+                return;
+            case "flame":
+                _lineRenderer = startPaintTrail(_materials[1]);
+                return;
+        }
+    }
+
+    private void OnTriggerExit2D() {
+        stopPaintTrail();
+    }
+
+
+}
